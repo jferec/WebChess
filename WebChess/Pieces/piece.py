d9b9ee20c7b8c4e936b29d4d7ad83fb435f06005
jferec <janferec@gmail.com>
2019-07-11 01:54:04 -0700

    piece interface

--- WebChess/Pieces/piece.py
+++ WebChess/Pieces/piece.py
@@ -5,11 +5,13 @@ class Piece:
     def __init__(self, pos, color):
         self._pos = pos
         self._color = color
+
     """
     Function returns all valid moves based solely
     on the piece's state. State of the chessboard is not
     taken into consideration.
     """
+
     @abc.abstractmethod
     def get_valid_moves(self):
         pass
@@ -17,10 +19,6 @@ class Piece:
     """
     Changes position of the piece.
     """
-    @abc.abstractmethod
+
     def move(self, pos):
-        pass
-
-
-
-
+        self._pos = pos
